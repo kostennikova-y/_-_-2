e2b7e5efc92d0d8204677e6eac9a5f375e19a5af
kostennikova-y <114347671+kostennikova-y@users.noreply.github.com>
2025-12-25 14:58:27 +0300

    Add test run for calculator operations

---
+++ main.py
@@ -0,0 +1,11 @@
+from calculator import add, sub, mul, div
+from memory import m_plus, mr, mc
+
+print(add(2, 3))
+print(sub(5, 2))
+print(mul(3, 4))
+print(div(10, 2))
+
+m_plus(10)
+print(mr())
+mc()
